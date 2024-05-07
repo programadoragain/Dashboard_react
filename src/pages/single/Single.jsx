f438da04729f93c1c18355cbbc5df95d9bc42b59
programadoragain <ferdandi04@gmail.com>
2024-05-07 01:06:20 -0300

    add single component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -9,8 +9,23 @@ const Single = () => {
       <Sidebar />
       <div className="single-container">
         <Navbar />
+        <div className='top'>
+          <div className='left'>
+            <div className='edit-button'>Edit</div>
+            <h2 className='title'>Information</h2>
+            <div className='item'>
+              <img src="https://img.freepik.com/fotos-premium/retrato-hermosa-joven-hispana-sonriendo-camara_900706-32539.jpg?size=626&ext=jpg" alt="" className="item-image" />
+              <div className='details'>
+                details
+              </div>
+            </div>
+          </div>
+          <div className='right'>
+            sasdasd
+          </div>
+        </div>
       </div>
-    </div>
+    </div>    
   )
 }
 
